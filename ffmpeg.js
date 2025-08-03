2ef6032b5ff4ad9378a9b0a83cb95931f8f23da2
Kumar Yaduvanshi <ahiratkumar999@gmail.com>
2025-08-04 04:40:02 +0530

    Removing unused code, files, or comments

--- ffmpeg.js
+++ ffmpeg.js
@@ -18,7 +18,7 @@ function startStreaming() {
     "-i", "anullsrc",
     "-shortest",
     "-c:v", "libx264",
-    "-b:v", "100k",
+    "-b:v", "200k",
     "-preset", "ultrafast",
     "-tune", "zerolatency",
     "-c:a", "aac",
